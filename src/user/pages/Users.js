9f90b84bec3417bd03d59df85a254c8a84ac6db7
Emin Kumru <eminkumru135@outlook.com>
2023-08-25 23:44:03 +0300

    styling and adding some components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h1>Users</h1>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Schwarz",
+      image:
+        "https://ceoworld.biz/wp-content/uploads/2022/05/4-Places-In-The-World-You-Must-See.jpg",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
